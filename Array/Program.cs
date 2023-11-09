4ddd45d9d42f8e67b1974944c90628cb99c0a0d9
Maheshvema <babumahesh2088@gmail.com>
2023-11-09 16:36:33 +0530

    modified

---
+++ Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, Avg = 0;
+            int[] Populate = new int[5];
+            for(int i = 1;i<=5;i++)
+            {
+                Console.WriteLine($"Enter the value {i} to find Average & Sum");
+                Console.ReadLine();
+            }
+            Console.WriteLine("Average of the numbers is:");
+           for(int i=0;i<=5;i++)
+            {
+                
+                sum += i;
+                Avg = sum / 5;
+            }
+            Console.WriteLine("Sum of the digits:" + sum);
+            Console.WriteLine("Average of the digits:" + Avg);
+            Console.ReadKey();
+        }
+    }
+}
